e0a61bd814e6ab017cd7ad43421c3a6ad71bd0f9
Fady Zarif <70828889+FadyZarif@users.noreply.github.com>
2025-11-15 00:45:38 +0200

    Add CocoaPods support and update iOS project configs
    Introduced Podfile and Podfile.lock to enable CocoaPods dependency management for the iOS project. Updated Debug.xcconfig and Release.xcconfig to include CocoaPods configuration includes. Modified Xcode project and workspace files to support the new dependency setup.

--- lib/features/user/logic/users_cubit.dart
+++ lib/features/user/logic/users_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
